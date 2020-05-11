ed5ab6ab04b778811cac988fa3a19ef80799d4c7
Brian Cai <briancai@u.northwestern.edu>
2020-05-10 21:37:24 -0400

    added a config.py file

--- src/config.py
+++ src/config.py
@@ -4,14 +4,14 @@ from os import path
 PROJECT_HOME = path.dirname(path.dirname(path.abspath(__file__)))
 
 # App config
-APP_NAME = "rapclassifier"
+APP_NAME = "isthisrap"
 DEBUG = True
 
 # Logging
 LOGGING_CONFIG = path.join(PROJECT_HOME, 'config/logging.conf')
 
 # Database connection config
-DATABASE_PATH = path.join(PROJECT_HOME, 'data/sportco.db')
+DATABASE_PATH = path.join(PROJECT_HOME, 'data/billboard_spotify.db')
 SQLALCHEMY_DATABASE_URI = 'sqlite:////{}'.format(DATABASE_PATH)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
@@ -23,5 +23,6 @@ API_SENTIMENT_PATH = 'sentiment'
 API_ENDPOINT = "http://{}:{}/{}".format(HOST, PORT, API_SENTIMENT_PATH)
 
 # Acquire and process config
-MAX_RECORDS_READ = 100
-SENTIMENT_RAW_LOCATION = path.join(PROJECT_HOME,'data/tweet_sentiment.json')
+# MAX_RECORDS_READ = 100
+BILLBOARD_RAW_LOCATION = path.join(PROJECT_HOME,'data/billboard.csv')
+SPOTIFY_RAW_LOCATION = path.join(PROJECT_HOME, 'data/spotify.csv')
